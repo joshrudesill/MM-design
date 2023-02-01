89968c25e747fc566c0d2a72ffb967d699c4e22f
Josh Rudesill <joshrudesill@gmail.com>
2023-02-01 19:33:02 +0100

    small stuff

--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,24 @@ export default function Home() {
   return (
     <>
       <Sidebar />
-      <div className='container mx-auto'>
-        <div className='grid ml-12 pt-12 pb-6 grid-rows-6 grid-cols-12 gap-4 h-screen'>
-          <div className='border row-span-3 col-span-5 flex justify-center'>
-            <p className='text-white text-2xl my-auto mx-auto'>Top Today</p>
+      <div className="container mx-auto">
+        <div className="grid ml-12 pt-12 pb-6 grid-rows-6 grid-cols-12 gap-3 h-screen">
+          <div className="border row-span-3 col-span-5 flex justify-center">
+            <p className="text-white text-2xl my-auto mx-auto">Top Today</p>
           </div>
-          <div className='border row-span-3 col-span-7 flex justify-center'>
-            <p className='text-white text-2xl my-auto mx-auto'>Graph</p>
+          <div className="border row-span-3 col-span-7 flex justify-center">
+            <p className="text-white text-2xl my-auto mx-auto">Graph</p>
           </div>
-          <div className='border row-span-3 col-span-4 flex justify-center'>
-            <p className='text-white text-2xl my-auto mx-auto'>
+          <div className="border row-span-3 col-span-4 flex justify-center">
+            <p className="text-white text-2xl my-auto mx-auto">
               Total Market View
             </p>
           </div>
-          <div className='border row-span-3 col-span-4 flex justify-center'>
-            <p className='text-white text-2xl my-auto mx-auto'>User Accuracy</p>
+          <div className="border row-span-3 col-span-4 flex justify-center">
+            <p className="text-white text-2xl my-auto mx-auto">User Accuracy</p>
           </div>
-          <div className='border row-span-3 col-span-4 flex justify-center'>
-            <p className='text-white text-2xl my-auto mx-auto'>Other</p>
+          <div className="border row-span-3 col-span-4 flex justify-center">
+            <p className="text-white text-2xl my-auto mx-auto">Other</p>
           </div>
         </div>
       </div>
