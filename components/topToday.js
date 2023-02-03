6d8efc7cc826d7b1cfccb7973a6eb9e3e314f498
Joshua Rudesill <joshrudesill@gmail.com>
2023-02-03 15:45:36 +0100

    scrollbar

--- components/topToday.js
+++ components/topToday.js
@@ -7,22 +7,22 @@ import TopTodayItem from "./topTodayItem";
 const TopToday = () => {
   return (
     <>
-      <div className="flex flex-col h-full">
-        <div className="flex justify-between border-b border-neutral-700 ">
+      <div className='flex flex-col h-full'>
+        <div className='flex justify-between border-b border-neutral-700 '>
           <p
             style={{ textShadow: "#0c6900 1px 1px 5px" }}
-            className="text-neutral-400 p-2 font-extralight text-md m-2 border rounded-md border-neutral-700 shadow-green-500/30 shadow-sm"
+            className='text-neutral-400 p-2 font-extralight text-md m-2 border rounded-md border-neutral-700 shadow-green-500/30 shadow-sm'
           >
             Top Today
           </p>
           <div
             style={{ textShadow: "#0c6900 1px 1px 5px" }}
-            className="text-white p-1 flex justify-center text-lg m-2 border rounded-md border-neutral-700 shadow-green-500/30 shadow-sm"
+            className='text-white p-1 flex justify-center text-lg m-2 border rounded-md border-neutral-700 shadow-green-500/30 shadow-sm'
           >
-            <RocketLaunchIcon className="h-6 my-auto animate-pulse text-green-600/50" />
+            <RocketLaunchIcon className='h-6 my-auto animate-pulse text-green-600/50' />
           </div>
         </div>
-        <div className="flex flex-col overflow-x-scroll text-sm gap-1 mt-2">
+        <div className='flex flex-col overflow-x-hidden text-sm gap-1 mt-2'>
           {[...Array(100).keys()].map((e) => (
             <TopTodayItem key={e} />
           ))}
