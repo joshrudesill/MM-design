583d6fa48fce739361122e1f9f7c5e9af1ae6a7b
Joshua Rudesill <joshrudesill@gmail.com>
2023-04-25 13:11:04 -0500

    search bar

--- components/sidebar.js
+++ components/sidebar.js
@@ -21,59 +21,59 @@ const Sidebar = () => {
   ];
   return (
     <div
-      className="flex flex-col group w-12 hover:w-56 border-r border-neutral-500/40 h-screen z-50 fixed left-0 "
+      className='flex flex-col group w-12 hover:w-56 border-r border-neutral-500/40 h-screen z-50 fixed left-0 '
       style={{ background: "rgb(38, 38, 38)" }}
     >
-      <div className="text-green-400  p-2 text-2xl border-green-700/20 border-b font-mono tracking-wide">
-        <p className="flex flex-row gap-1">
-          <CurrencyDollarIcon className="h-8" />
-          <p className="hidden group-hover:block">MonkeeMath</p>
-        </p>
+      <div className='text-green-400  p-2 text-2xl border-green-700/20 border-b font-mono tracking-wide'>
+        <div className='flex flex-row gap-1'>
+          <CurrencyDollarIcon className='h-8' />
+          <p className='hidden group-hover:block'>MonkeeMath</p>
+        </div>
       </div>
-      <div className=" text-green-500 p-3 transition transform hover:-translate-y-0.5 cursor-pointer ">
-        <p
+      <div className=' text-green-500 p-3 transition transform hover:-translate-y-0.5 cursor-pointer '>
+        <div
           style={{ textShadow: "#0c6900 1px 1px 5px" }}
-          className="flex flex-row gap-2"
+          className='flex flex-row gap-2'
         >
-          <HomeIcon className="h-6" />
-          <p className="hidden group-hover:block">{names[0]}</p>
-        </p>
+          <HomeIcon className='h-6' />
+          <p className='hidden group-hover:block'>{names[0]}</p>
+        </div>
       </div>
-      <div className="text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer ">
-        <p className="flex flex-row gap-2">
-          <ChatBubbleBottomCenterTextIcon className="h-6" />
-          <p className="hidden group-hover:block">{names[1]}</p>
-        </p>
+      <div className='text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer '>
+        <div className='flex flex-row gap-2'>
+          <ChatBubbleBottomCenterTextIcon className='h-6' />
+          <p className='hidden group-hover:block'>{names[1]}</p>
+        </div>
       </div>
-      <div className="text-neutral-400  p-3 transition transform hover:-translate-y-0.5 cursor-pointer  ">
-        <p className="flex flex-row gap-2">
-          <ArrowTrendingUpIcon className="h-6" />
-          <p className="hidden group-hover:block">{names[2]}</p>
-        </p>
+      <div className='text-neutral-400  p-3 transition transform hover:-translate-y-0.5 cursor-pointer  '>
+        <div className='flex flex-row gap-2'>
+          <ArrowTrendingUpIcon className='h-6' />
+          <p className='hidden group-hover:block'>{names[2]}</p>
+        </div>
       </div>
-      <div className="text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer ">
-        <p className="flex flex-row gap-2">
-          <LifebuoyIcon className="h-6" />
-          <p className="hidden group-hover:block">{names[3]}</p>
-        </p>
+      <div className='text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer '>
+        <div className='flex flex-row gap-2'>
+          <LifebuoyIcon className='h-6' />
+          <p className='hidden group-hover:block'>{names[3]}</p>
+        </div>
       </div>
-      <div className="text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer ">
-        <p className="flex flex-row gap-2">
-          <CalculatorIcon className="h-6" />
-          <p className="hidden group-hover:block">{names[4]}</p>
-        </p>
+      <div className='text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer '>
+        <div className='flex flex-row gap-2'>
+          <CalculatorIcon className='h-6' />
+          <p className='hidden group-hover:block'>{names[4]}</p>
+        </div>
       </div>
-      <div className="text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer ">
-        <p className="flex flex-row gap-2">
-          <ScaleIcon className="h-6" />
-          <p className="hidden group-hover:block">{names[5]}</p>
-        </p>
+      <div className='text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer '>
+        <div className='flex flex-row gap-2'>
+          <ScaleIcon className='h-6' />
+          <p className='hidden group-hover:block'>{names[5]}</p>
+        </div>
       </div>
-      <div className="text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer ">
-        <p className="flex flex-row gap-2">
-          <UsersIcon className="h-6" />
-          <p className="hidden group-hover:block">{names[6]}</p>
-        </p>
+      <div className='text-neutral-400  p-3  transition transform hover:-translate-y-0.5 cursor-pointer '>
+        <div className='flex flex-row gap-2'>
+          <UsersIcon className='h-6' />
+          <p className='hidden group-hover:block'>{names[6]}</p>
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import Graph from "@/components/graph";
 import Other from "@/components/otherfeatures";
 import Sidebar from "@/components/sidebar";
 import TopToday from "@/components/topToday";
+import {
+  MagnifyingGlassIcon,
+  ServerStackIcon,
+} from "@heroicons/react/24/outline";
 
 import { useRef } from "react";
 export default function Home() {
@@ -11,7 +15,16 @@ export default function Home() {
     <>
       <Sidebar />
       <div className='container mx-auto'>
-        <div className='grid ml-12 pt-12 pb-6 grid-rows-6 grid-cols-12 gap-3 sm:h-screen'>
+        <div className='flex mt-3 ml-12'>
+          <input
+            className='w-full bg-neutral-600 p-2 rounded-l-md text-neutral-900'
+            placeholder='Search symbols here'
+          ></input>
+          <div className=' bg-neutral-700 p-2 rounded-r-md'>
+            <MagnifyingGlassIcon className='h-6 my-auto text-green-600' />
+          </div>
+        </div>
+        <div className='grid ml-12 pt-8 pb-6 grid-rows-6 grid-cols-12 gap-3 sm:h-screen'>
           <div className='rounded-lg hover:shadow-green-400/80 shadow-md row-span-3 md:col-span-8 col-span-full sm:col-span-6 bg-neutral-800'>
             <TopToday />
           </div>
